a4acb340a6c445679663e93232f66d7702be111b
har2525life <har2525life@outlook.com>
2023-06-12 15:42:32 +0900

    add: head data

---
+++ app/head.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Head = () => {
+  return (
+    <>
+        <title>Blog</title>
+        <meta content='width=device-width, initial-scale=1' name="viewport" />
+        <meta content="Next.js Supabase Blog" name="description" />
+        <link rel="icon" href="/favicon.ico" />
+    </>
+  )
+}
+
+export default Head
