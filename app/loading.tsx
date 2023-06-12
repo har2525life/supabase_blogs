46043489838bc8e55a100710523e93d896004211
har2525life <har2525life@outlook.com>
2023-06-12 16:02:50 +0900

    add: loading page

---
+++ app/loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className="flex justify-center">
+        <div className="h-10 w-10 animate-spin rounded-full border-4 border-yellow"></div>
+    </div>
+  )
+}
+
+export default Loading
